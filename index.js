0d6a376b5894f735dcf5f59274b3764a7d4bf00c
Darwin Luque <darwin.luque.98@gmail.com>
2021-02-22 23:14:40 -0600

    Read the trimmed path of the url and the method used

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-/**
- * Primary file for the API
- */
+/*
+* Primary file for the API
+*/
 
- // Dependecies
- const http = require('http');
+// Dependecies
+const http = require('http');
+const url = require('url');
 
 // The server should respond to all requests with a string
 const server = http.createServer(function(req, res) {
+    // Get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl.pathname
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the HTTP Method
+    const method = req.method.toLowerCase();
+
+    // Send the reponse
     res.end('Hello World\n');
+
+    // Log the request path
+    console.log(`Request received on this path: ${trimmedPath}. The method was: ${method}`);
 });
 
 // Start the server, and have it listen on port 5050
