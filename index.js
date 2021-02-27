caf9a8f073cd8355a75e3a007d9714c12b800e7e
Darwin Luque <darwin.luque.98@gmail.com>
2021-02-26 20:37:37 -0600

    /checks and /users handlers integrated

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 const { StringDecoder } = require('string_decoder');
-const config = require('./config');
+const config = require('./lib/config');
 const fs = require('fs');
+const handlers = require('./lib/handlers');
+const helpers = require('./lib/helpers');
 
 // Instantiate HTTP server
 const httpServer = http.createServer((req, res) => {
@@ -71,11 +73,11 @@ const unifiedServer = (req, res) => {
 
     // Construct the data object to send to the handler
     const data = {
-      trimmedPath: trimmedPath,
-      queryStringObject: queryStringObject,
-      method: method,
-      headers: headers,
-      payload: buffer,
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: helpers.parseJsonToObject(buffer),
     };
 
     // Route the request to the handler specified in the router
@@ -100,20 +102,10 @@ const unifiedServer = (req, res) => {
   });
 };
 
-// Define the handlers
-const handlers = {};
-
-// Sample handler
-handlers.ping = (data, callback) => {
-  callback(200);
-};
-
-// Not found handler
-handlers.notFound = (data, callback) => {
-  callback(404);
-};
-
 // Define a request router
 const router = {
   ping: handlers.ping,
+  users: handlers.users,
+  tokens: handlers.tokens,
+  checks: handlers.checks,
 };
